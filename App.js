f748a46e4ed5a6d5b031e9abd4dcdc9d6455d837
Nikul <nikul@trustedcommunities.org>
2022-11-07 15:59:51 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {NativeBaseProvider} from 'native-base';
+import Home from './src/screens/Home';
+
+export default function App() {
+  return (
+    <NativeBaseProvider>
+      <Home />
+    </NativeBaseProvider>
+  );
+}
---
+++ src/screens/Home/index.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import {Box} from 'native-base';
+
+export default function Home() {
+  return (
+    <Box>Nikul</Box>
+  );
+}
